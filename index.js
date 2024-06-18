bbea9db6ef37d239f29ae1d17435330475a69ad5
DJ Mountney <david@twkie.net>
2024-06-17 17:29:21 -0700

    Fix several issues with the module type
    - Pass the correct PR data

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import * as core from "@actions/core";
 import { context, getOctokit } from "@actions/github";
-import { Trafico } from "./lib/trafico";
+import { Trafico } from "./lib/trafico.js";
 
 async function runTrafico() {
   try {
-    const token = core.getInput("github-token");
+    // Swap token to a getInput once we build for dist
+    const token = process.env.GHTOKEN;
     const { owner, repo } = context.repo;
     const { rest } = getOctokit(token);
     const trafico = new Trafico(rest, { owner, repo });
@@ -15,7 +16,7 @@ async function runTrafico() {
       pull_number: context.payload.pull_request.number
     });
 
-    trafico.runTrafico(context, pullRequest, {});
+    trafico.runTrafico(context, pullRequest.data, {});
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message);
   }
